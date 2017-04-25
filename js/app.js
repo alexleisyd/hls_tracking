7c03aa9d2a22a9f010d84a2f0bfe9457060b65e7
Alex Lei <luckyhokey@gmail.com>
2017-04-25 19:52:03 +1000

    2nd Preview

--- js/app.js
+++ js/app.js
@@ -1,15 +1,65 @@
 (function(){
-	var app=angular.module('mainApp',['ngAnimate']);
-    app.config(['$locationProvider', function($locationProvider){
+	var app=angular.module('mainApp',['ngAnimate','pascalprecht.translate','ngSanitize']);
+    app.config(['$locationProvider','$translateProvider', function($locationProvider, $translateProvider){
         $locationProvider.html5Mode({
             enabled: true,
             requireBase: false
         });
+
+        $translateProvider.useSanitizeValueStrategy('escape');
+
+        $translateProvider.translations('en',{
+            TITLE:'Customer Delivery Tracking Portal',
+            SUBTITLE:'Track a delivery',
+            PICKINGSLIP:'Picking Slip',
+            PICKINGSLIP_INVALID:'Invalid Picking Slip',
+            NOTE:'Please enter the picking slip number and suburb',
+            SUBURB:'Suburb',
+            SUBURB_INVALID:'Invalid Suburb',
+            SUBMIT:'Track',
+            CLEAR:'Clear',
+            ERROR:'Error',
+            NORECORD:'No record found matching the Picking Slip and suburb. Please try again',
+            CHECK_ANOTHER:'Check another delivery',
+            ORDER_NO:'Order Number',
+            STATUS:'Status',
+            EXPECTED_DATE:'Expected Date',
+            SCHEDULE:'Schedule',
+            STEP_1:'Order Received',
+            STEP_2:'Order Prepared',
+            STEP_3:'Delivery Scheduled',
+            STEP_4:'Order Dispatched',
+            STEP_5:'Order Delivered'
+        }).translations('cn',{
+            TITLE:'物流追踪查询平台',
+            SUBTITLE:'查询订单',
+            PICKINGSLIP:'订单号',
+            PICKINGSLIP_INVALID:'订单号无效',
+            NOTE:'请输入订单号及地区名查询',
+            SUBURB:'地区',
+            SUBURB_INVALID:'区名无效',
+            SUBMIT:'查询',
+            CLEAR:'清除',
+            ERROR:'错误',
+            NORECORD:'未找到符合订单号及区名的记录。请重试。',
+            CHECK_ANOTHER:'查询另一个订单',
+            ORDER_NO:'订单号',
+            STATUS:'状态',
+            EXPECTED_DATE:'预计送达',
+            SCHEDULE:'日程',
+            STEP_1:'订单确认',
+            STEP_2:'订单就绪',
+            STEP_3:'送货排期',
+            STEP_4:'订单发货',
+            STEP_5:'订单送达'
+        });
+        $translateProvider.preferredLanguage('en');
+
     }]);
 
-	app.controller('mainCtrl',['$http','$location',function($http,$location){
+	app.controller('mainCtrl',['$http','$location','$translate',function($http,$location,$translate){
         var ctrl=this;
-
+        ctrl.language='en';
 
 
         ctrl.initEntry=function(){
@@ -52,7 +102,7 @@
 
             var req={
                 method: 'GET',
-                url: 'http://192.168.3.250:8080/HLS-REST/tracking?pickingSlip='+encodeURIComponent(ctrl.pickingSlip)+"&suburb="+encodeURIComponent(ctrl.suburb),
+                url: 'http://tracking.humeplaster.com.au:8080/HLS-REST/tracking?pickingSlip='+encodeURIComponent(ctrl.pickingSlip)+"&suburb="+encodeURIComponent(ctrl.suburb),
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization':'Basic YWRtaW46YWRtaW4='
@@ -81,6 +131,10 @@
             ctrl.pickingSlip='';
             ctrl.suburb='';
         };
+
+        ctrl.updateLanguage = function(lan){
+            $translate.use(lan);
+        }
 
         if (ctrl.pickingSlip === undefined && ctrl.suburb === undefined){
 
